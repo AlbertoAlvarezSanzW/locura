444d72ab289116c22f47f6ad13a0412458cf6737
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-26 14:24:11 +0100

    commit

--- app/src/main/java/com/example/copiaa/MainActivity.kt
+++ app/src/main/java/com/example/copiaa/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // para añadir el "nav_items" hemos de crear una carpeta menu, en esa carpeta tenemos que crear un nuevo archivo que se llamara "nav_items"
+        // despues es ir poniendo las imagenes en drawable y poniendo los id's, luego insertamos:
+        // app:menu="@menu/nav_items"
     }
 }
